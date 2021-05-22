10e076794ae5d8b0ed1edeec5f0b14c2a3c1ef46
Chris <cteters@gmail.com>
2021-05-22 15:24:00 -0700

    Add project files.

---
+++ PowerPoint_Creation_Tool/ImageSearch.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+
+namespace PowerPointWPF
+{
+    /// <summary>
+    /// Interaction logic for ImageSearch.xaml
+    /// </summary>
+
+
+    // strongly typed object for json parsing, not currently utilized
+    public class Thumbnail
+    {
+        public string src { get; set; }
+    }
+
+    // strongly typed object for json parsing, not currently utilized
+    public class CseThumbnail
+    {
+        public string src { get; set; }
+    }
+
+    // strongly typed object for json parsing, not currently utilized
+    public class Pagemap
+    {
+        public List<CseThumbnail> cse_thumbnail { get; set; }
+        public List<Thumbnail> thumbnail { get; set; }
+    }
+
+    public partial class ImageSearch : Window
+    {
+        private string CX = "";
+        private string APIKEY = "";
+        private string text;
+        private List<string> thumbnails;
+        private List<string> selected;
+        //private Object[] itemSelections;
+        public ImageSearch()
+        {
+            InitializeComponent();
+        }
+
+        public ImageSearch(string text)
+        {
+            InitializeComponent();
+            string json;
+
+            var request = WebRequest.Create("https://www.googleapis.com/customsearch/v1?key=" + APIKEY + "&cx=" + CX + "&q=" + text);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            JObject data = JObject.Parse(json);
+
+            dynamic foo = JObject.Parse(json);
+            thumbnails = new List<string>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                thumbnails.Add(foo.items[i].pagemap.cse_image[0].src.ToString());
+            }
+
+            imagePopulate();
+        }
+
+        public void imagePopulate()
+        {
+            for (int i = 0; i < thumbnails.Count; i++)
+            {
+                BitmapImage original = new BitmapImage();
+                original.BeginInit();
+                original.UriSource = new Uri(thumbnails[i]);
+                original.DecodePixelWidth = 200;
+                original.EndInit();
+
+                Image final = new Image();
+                final.Source = original;
+
+                //Object[] itemSelections = new Object[] { new System.Windows.Controls.CheckBox(), final };
+                //imageList.Items.Add(itemSelections);
+                imageList.Items.Add(final);
+                imageList.Items.Add(new CheckBox());
+            }
+        }
+
+        public List<string> getSelected()
+        {
+            return selected;
+        }
+
+        private void SubmitClick(object sender, RoutedEventArgs e)
+        {
+            selected = new List<string>();
+
+            for (int i = 1; i < imageList.Items.Count; i += 2)
+            {
+                //Object[] itemSelection = new Object[] { imageList.Items.GetItemAt(i) };
+                //System.Windows.Controls.CheckBox checkbox = (System.Windows.Controls.CheckBox)itemSelection[0];
+                imageList.Items.GetItemAt(i);
+                CheckBox checkbox = (CheckBox)imageList.Items.GetItemAt(i);
+                if (checkbox.IsChecked == true)
+                {
+                    Image image = (Image)imageList.Items.GetItemAt(i - 1);
+                    selected.Add(image.Source.ToString());
+                }
+            }
+
+            Close();
+        }
+
+        private void CancleClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ PowerPoint_Creation_Tool/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Office.Interop.PowerPoint;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Core;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+// solution starting point:
+// https://stackoverflow.com/questions/26372020/how-to-programmatically-create-a-powerpoint-from-a-list-of-images
+
+
+namespace PowerPointWPF
+{
+    public partial class MainWindow : Window
+    {
+        private Microsoft.Office.Interop.PowerPoint.Application pptApplication;
+        private Slides slides;
+        private _Slide slide;
+        private TextRange objText;
+        private CustomLayout customLayout;
+        public List<string> selected;
+        private Presentation pptPresentation;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pptApplication = new Microsoft.Office.Interop.PowerPoint.Application();
+
+            // Create the Presentation File
+            pptPresentation = pptApplication.Presentations.Add(MsoTriState.msoTrue);
+            customLayout = pptPresentation.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutText];
+        }
+
+        private void PhotosClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Documents.TextRange textRange = new System.Windows.Documents.TextRange
+                (
+                    Text.Document.ContentStart, // TextPointer to the start of content in the RichTextBox.          
+                    Text.Document.ContentEnd    // TextPointer to the end of content in the RichTextBox.
+                );
+
+            string convertedText = Regex.Replace(textRange.Text, "\\s+", "+");
+
+            ImageSearch newWindow = new ImageSearch(convertedText);
+            newWindow.ShowDialog();
+            selected = newWindow.getSelected();
+        }
+
+        private void SubmitClick(object sender, RoutedEventArgs e)
+        {
+            // Create new Slide
+            slides = pptPresentation.Slides;
+            slide = slides.AddSlide(1, customLayout);
+
+            // Add title
+            objText = slide.Shapes[1].TextFrame.TextRange;
+            System.Windows.Documents.TextRange titleRange = new System.Windows.Documents.TextRange
+            (
+                Title.Document.ContentStart, // TextPointer to the start of content in the RichTextBox.          
+                Title.Document.ContentEnd    // TextPointer to the end of content in the RichTextBox.
+            );
+            objText.Text = titleRange.Text;
+
+            // Add text
+            objText = slide.Shapes[2].TextFrame.TextRange;
+            System.Windows.Documents.TextRange textRange = new System.Windows.Documents.TextRange
+            (
+                Text.Document.ContentStart, // TextPointer to the start of content in the RichTextBox.          
+                Text.Document.ContentEnd    // TextPointer to the end of content in the RichTextBox.
+            );
+            objText.Text = textRange.Text;
+
+            int numPics = 0;
+            if (selected != null)
+            {
+                numPics = selected.Count();
+            }
+
+            for (int i = 0; i < numPics && i < 3; i++)
+            {
+                slide.Shapes.AddPicture(selected[i], MsoTriState.msoFalse, MsoTriState.msoTrue, ((i + 1) * 200), 300, 175, 175);
+            }
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
